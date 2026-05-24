ddb3c3181d6990f2f5a8ca7c3219b64854c07635
EverJurado <ejuradoj@fcpn.edu.bo>
2026-05-24 14:51:45 -0400

    errores v3

--- backend/src/database/connectionPostgresql.js
+++ backend/src/database/connectionPostgresql.js
@@ -1,12 +1,21 @@
-import { Pool } from "pg";
+import pkg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD || undefined,
-  port: Number(process.env.DB_PORT),
-});
+const { Pool } = pkg;
+
+export const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    })
+  : new Pool({
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_DATABASE,
+      password: process.env.DB_PASSWORD || undefined,
+      port: Number(process.env.DB_PORT),
+    });
